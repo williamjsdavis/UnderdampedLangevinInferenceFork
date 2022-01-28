b1d65e3c86496ac6bf77af1153c2eba88306e7be
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2022-01-28 14:01:14 -0800

    Change path of UnderdampedLangevinProcess

--- underdampedlangevininference/__init__.py
+++ underdampedlangevininference/__init__.py
@@ -1,4 +1,4 @@
-from .ULI_langevin import UnderdampedLangevinProcess
+from .ULI_langevin.ULI_langevin import UnderdampedLangevinProcess
 from .ULI_data import StochasticTrajectoryData
 from .ULI_inference import UnderdampedLangevinInference
 from .ULI_plotting_toolkit import *
